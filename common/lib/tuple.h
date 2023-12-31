2993239a366652a75091c5f235b03bc46c9294bb
Kuba Marchut <kuba.marchut@gmail.com>
2023-12-31 22:29:11 +0100

    copying source code (exampple)

---
+++ common/lib/tuple.h
@@ -0,0 +1,63 @@
+#ifndef TUPLE_SPACE_H
+2.
+#define TUPLE_SPACE_ H
+3.
+#define TS_YES
+1
+4.
+#define TS_NO
+0
+5.
+#define TS_INT
+0
+6.
+#define TS_FLOAT
+1
+7.
+#define TS_SUCCESS
+1
+8.
+#define TS_FAILURE
+0
+9.
+typedef
+struct
+10.
+int
+is_actual ;;/* does the data member contains data */
+11.
+int
+type ;;/* what is the type of the data member
+12.
+union
+13.
+int
+int_field
+14.
+float
+float_field
+15.
+}
+data;
+16.
+}
+field_t ;;/* a new type corresponding to one field of a tuple
+17.
+/* API */
+18.
+/*
+these functions return TS_SUCCESS or TS_FAILURE */
+19.
+/*
+parameters : tuple name , other fields , no. of other fields
+20.
+int
+ts_out (char*, field_t *, int
+21.
+int
+ts_inp (char*, field_t *, int
+22.
+int
+ts_rdp (char*, field_t *, int
+23.
+#endif
