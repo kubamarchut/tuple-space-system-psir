c54aa8acddec23c097c48407a19ced70ede7cf7a
Kuba Marchut <kuba.marchut@gmail.com>
2023-12-31 22:38:01 +0100

    fixing formating spaces

--- common/lib/tuple.h
+++ common/lib/tuple.h
@@ -1,21 +1,21 @@
 #ifndef TUPLE_SPACE_H
 #define TUPLE_SPACE_H
 
-#define TS_YES 1
-#define TS_NO 0
-#define TS_INT 0
-#define TS_FLOAT 1
-#define TS_SUCCESS 1
-#define TS_FAILURE 0
+#define TS_YES      1
+#define TS_NO       0
+#define TS_INT      0
+#define TS_FLOAT    1
+#define TS_SUCCESS  1
+#define TS_FAILURE  0
 
 typedef struct {
-    int is_actual; /* does the data member contains data */
-    int type;     /* what is the type of the data member */
+    int is_actual;  /* does the data member contains data */
+    int type;       /* what is the type of the data member */
     union {
         int int_field;
         float float_field;
     } data;
-} field_t; /* a new type corresponding to one field of a tuple*/
+} field_t;          /* a new type corresponding to one field of a tuple*/
 
 /* API */
 /* these functions return TS_SUCCESS or TS_FAILURE */
