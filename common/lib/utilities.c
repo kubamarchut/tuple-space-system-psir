5ffd4a209c9317477109066dfe95943b9db2d792
Kuba Marchut <kuba.marchut@gmail.com>
2024-01-01 18:32:22 +0100

    adding some basic functions lib

---
+++ common/lib/utilities.c
@@ -0,0 +1,19 @@
+#include "utilities.h"
+#include <sys/time.h>
+#include <time.h>
+
+void printTimestamp() {
+    struct timeval current_time;
+    gettimeofday(&current_time, NULL);
+
+    struct tm *local_time = localtime(&current_time.tv_sec);
+    char time_string[20];
+    strftime(time_string, sizeof(time_string), "%x %H:%M:%S", local_time);
+
+    printf("%s.%03ld", time_string, (long int)current_time.tv_usec / 1000);
+}
+
+void printError(int line, char *filename, char* message) {
+    printTimestamp();
+    printf(" - ERROR occured (%s:%d) %s\n", filename, line, message);
+}
---
+++ common/lib/utilities.h
@@ -0,0 +1,8 @@
+#ifndef UTILITIES_H
+#define UTILITIES_H
+
+void printTimestamp() {}
+
+void printError(int line, char *filename, char* message) {}
+
+#endif /* UTILITIES_H */
