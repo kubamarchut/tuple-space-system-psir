5389af38dfbc1fd05596bec1d18fb16d40f6b7e2
Kuba Marchut <kuba.marchut@gmail.com>
2024-01-01 18:32:05 +0100

    trying to implement some basic udp handlers

--- common/lib/udp_handler.c
+++ common/lib/udp_handler.c
@@ -1,4 +1,5 @@
 #include "udp_handler.h"
+#include "utilities.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,31 +8,73 @@
 #include <unistd.h>
 
 int udp_socket = -1; // Definition of the UDP socket
+struct addrinfo *r = NULL;
 
-int init_udp_socket() {
-    udp_socket = socket(AF_INET, SOCK_DGRAM, 0);
-    if (udp_socket < 0) {
-        perror("Socket creation failed");
-        return -1;
+int init_udp_socket(){
+    struct addrinfo h;
+
+    memset(&h, 0, sizeof(struct addrinfo));
+    h.ai_family = PF_INET;
+    h.ai_socktype = SOCK_DGRAM;
+    h.ai_flags = AI_PASSIVE;
+
+    if (getaddrinfo(CLIENT_IP, PORT, &h, &r) != 0)
+    {
+        printError(__LINE__, __FILE__, strerror(errno));
+        exit(-1);
     }
-    
-    // Additional setup if needed
-    
-    return 0;
+    printTimestamp();
+    printf(" - getaddrinfo successful\n");
+
+    if ((udp_socket = socket(r->ai_family, r->ai_socktype, r->ai_protocol)) == -1)
+    {
+        printError(__LINE__, __FILE__, strerror(errno));
+        exit(-1);
+    }
+    printTimestamp();
+    printf(" - created new socket successfully\n");
+
+    return udp_socket;
 }
 
-int send_udp_packet(char* buffer, int length) {
-    // Implementation logic for sending UDP packets
+int send_udp_packet(char *buffer, int length){
+    unsigned char message[length];
+
+    snprintf(message, length, "%s", buffer);
+    int pos = sendto(udp_socket,
+                     message,
+                     length,
+                     0, r->ai_addr, r->ai_addrlen);
+    if (pos < 0)
+    {
+        printError(__LINE__, __FILE__, strerror(errno));
+        exit(-1);
+    }
+    else
+    {
+        printTimestamp();
+        printf(" - sending a message\n");
+    }
+    return pos;
+}
+
+int receive_udp_packet(char *buffer, int length){
+    struct sockaddr_in c;
+    int pos, c_len = sizeof(c);
+    if ((pos = recvfrom(s, buffer, length, 0,
+                        (struct sockaddr *)&c, &c_len)) < 0
+                       ){
+        printError(__LINE__, __FILE__, strerror(errno));
+        exit(-1);
+    }
+    received_command[pos] = '\0';
     return 0; // Placeholder, replace with actual logic
 }
 
-int receive_udp_packet(char* buffer, int length) {
-    // Implementation logic for receiving UDP packets
-    return 0; // Placeholder, replace with actual logic
-}
-
-void close_udp_socket() {
-    if (udp_socket != -1) {
+void close_udp_socket()
+{
+    if (udp_socket != -1)
+    {
         close(udp_socket);
         udp_socket = -1;
     }
