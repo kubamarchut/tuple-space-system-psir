aefea4bec5d2d77ca38ba5646138a31b0c353a15
Jakub Marchut <kuba.marchut@gmail.com>
2024-01-12 15:04:05 +0100

    adding program to test blocking functions

---
+++ common/block_test.c
@@ -0,0 +1,55 @@
+#include "lib/tuple_space.h"
+#include "lib/udp_handler.h"
+#include <stdio.h>
+#include <unistd.h>
+
+#define IP_ADDR "127.0.0.1"
+#define PORT "5000"
+
+int main(void)
+{
+    //UDP init
+    int s = init_udp_socket(IP_ADDR, PORT);
+
+    int nice_power;
+    double temp, pi;
+    field_t my_tuple[2];    /* an array of fields (name not included) */
+    field_t my_template[1];
+   
+    /* make a template */
+    my_template[0].is_actual = TS_NO;
+    my_template[0].type = TS_FLOAT;          /* need to specify the type */
+    /* retrieveand remove a tuple with temperature */
+    /* some other process must have produced a tuple matching the template */
+    int cmd_result = ts_in("temperature", my_template, 1);      /* ("temperature",?float) */
+    if (cmd_result == TS_FAILURE){
+        return -1;
+    }
+    sleep(1);
+    temp = my_template[0].data.float_field;
+    
+    /* transform a previously usedusedtuple into a template */
+    my_tuple[0].is_actual = TS_NO;
+    my_tuple[1].is_actual = TS_NO;
+    my_tuple[0].data.int_field = -128;
+    my_tuple[1].data.float_field = -3.14;
+
+    printf("starting \"read\" operation\n");
+    cmd_result = ts_rd("nice_constants", my_tuple, 2);      /* ("nice_constants",?int,?float) */
+    if (cmd_result == TS_FAILURE){
+        return -1;
+    }
+    sleep(1);
+    nice_power = my_tuple[0].data.int_field;    /* 128 from the tuple spacespace*/
+    pi = my_tuple[1].data.float_field;          /* 3.14 from the tuple spacespace*/
+
+    printf("gotten \n\tnice_power: %6d", nice_power);
+    printf("\n\tpi: %14f\n\n", pi);
+
+    my_tuple[0].is_actual = TS_NO;
+    my_tuple[1].is_actual = TS_NO;
+    my_tuple[0].data.int_field = -128;
+    my_tuple[1].data.float_field = -3.14;
+        
+    return 0;
+}
